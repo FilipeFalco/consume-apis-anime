9e04e861ed2fd0203d841fb3494b3640af205061
Filipe Ferreira Falco <falcoefilipe@hotmail.com>
2021-10-09 22:44:17 -0400

    Retorno das apis funcionais, parcer de json para csv com problema

--- src/app.js
+++ src/app.js
@@ -1,12 +1,52 @@
 import axios from 'axios';
+import jsonToCsv from 'json2csv';
 
 class Api {
 
-    static anime(nome){
-        axios.defaults.headers.common['header1'] = { Authorization: `Bearer ${}`}
-        const response = axios.get(`${process.env.MYANIMELIST_BASE_URL}/anime?q=${nome}`);
-        console.log('response: ', response);
+    static async news() {
+        const response = (await axios.get(`https://inshortsapi.vercel.app/news?category=science`)).data;
+        console.log('response news: ', response);
+    }
+
+    static async spaceFlight() {
+        const response = (await axios.get(`https://api.spaceflightnewsapi.net/v3/articles?_limit=20`)).data
+        console.log('response spaceFlight: ', response)
+    }
+
+    static async waifu() {
+        const response = (await axios.get(`https://api.waifu.pics/sfw/neko`)).data
+        
+        console.log('response waifu: ', json2csv(response))
+    }
+
+    static json2csv(json) {
+        let propriedades = [];
+        let primeiraLinha = JSON.stringify(json[0]).split(",")
+        for (item of primeiraLinha) {
+            let nomes = item.split('"');
+            propriedades.push(nomes[1]);
+        }
+
+        var prototipo_csv = [];
+        prototipo_csv[0] = '';
+        for (prop of propriedades) {
+            prototipo_csv[0] += '"' + prop + '",';
+        }
+
+        for (var i = 0; i < json.length; i++) {
+            prototipo_csv[i + 1] = '';
+            for (prop of propriedades) {
+                prototipo_csv[i + 1] += '"' + json[i][prop] + '",';
+            }
+        }
+
+        var csv_result = '';
+        for (item of prototipo_csv) {
+            csv_result += item + "\n";
+        }
+
+        return csv_result;
     }
 }
 
-Api.anime('myhero');
+Api.waifu();
