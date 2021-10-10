01d0cdf5812f92876832d98b18b1fbd5c9e8408a
Filipe Ferreira Falco <falcoefilipe@hotmail.com>
2021-10-10 15:26:42 -0400

    projeto finalizado e funcional, conversao do json para csv funcionando

--- src/app.js
+++ src/app.js
@@ -1,52 +1,32 @@
 import axios from 'axios';
-import jsonToCsv from 'json2csv';
+import csvjson from 'csvjson';
+import fs from 'fs';
 
 class Api {
 
     static async news() {
         const response = (await axios.get(`https://inshortsapi.vercel.app/news?category=science`)).data;
-        console.log('response news: ', response);
+        Api.criarArquivo(response)
     }
 
     static async spaceFlight() {
         const response = (await axios.get(`https://api.spaceflightnewsapi.net/v3/articles?_limit=20`)).data
-        console.log('response spaceFlight: ', response)
+        Api.criarArquivo(response)
     }
 
     static async waifu() {
         const response = (await axios.get(`https://api.waifu.pics/sfw/neko`)).data
-        
-        console.log('response waifu: ', json2csv(response))
+        Api.criarArquivo(response)
     }
 
-    static json2csv(json) {
-        let propriedades = [];
-        let primeiraLinha = JSON.stringify(json[0]).split(",")
-        for (item of primeiraLinha) {
-            let nomes = item.split('"');
-            propriedades.push(nomes[1]);
-        }
-
-        var prototipo_csv = [];
-        prototipo_csv[0] = '';
-        for (prop of propriedades) {
-            prototipo_csv[0] += '"' + prop + '",';
-        }
-
-        for (var i = 0; i < json.length; i++) {
-            prototipo_csv[i + 1] = '';
-            for (prop of propriedades) {
-                prototipo_csv[i + 1] += '"' + json[i][prop] + '",';
-            }
-        }
-
-        var csv_result = '';
-        for (item of prototipo_csv) {
-            csv_result += item + "\n";
-        }
-
-        return csv_result;
+    static criarArquivo(text) {
+        fs.writeFile('arquivo.csv', csvjson.toCSV(text), (err) => {
+            if (err) throw err;
+            console.log('Arquivo criado com sucesso.');
+        });
     }
 }
 
-Api.waifu();
+//Api.waifu();
+Api.news();
+//Api.spaceFlight();
